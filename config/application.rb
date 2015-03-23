bcc59b0d375090c70507cdb3321a9c17c73ee63f
Noémien Kocher <nkcr.je@gmail.com>
2015-03-23 13:03:59 +0100

    added index

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,16 @@ module JstechCh
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Disable generators
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework  :rspec, fixture: false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+    end
+    
   end
 end
