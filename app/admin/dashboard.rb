3fd0832bf5d90d68b9ae9a205e1361097aefb3f2
khcr <kocher.ke@gmail.com>
2015-09-28 19:02:30 +0200

    active admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,12 +5,9 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc{ I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+        span "JS-Tech administration"
       end
     end
-
-    div "JS-Tech administration"
 
     # Here is an example of a simple dashboard with columns and panels.
     #
